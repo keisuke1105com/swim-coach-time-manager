f3b40fbfc46e919e4aa536085b921ccac0834d64
keisuke1105com <keisuke1105.com@gmail.com>
2026-09-12 20:06:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Supabase 接続設定
 window.SUPABASE_URL = "https://rzepnhdlzsygckxkptgf.supabase.co";
-window.SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_3xFHoG5zKvyKbKhK304JAQ_PedL8y5g";
